d082514c853e82ba232d04f17112a150fa50cb2b
lulinqing <lulinqing@foxmail.com>
2016-05-30 17:11:20 +0800

    修改

---
+++ source/OCEloquent/OCEloquent/FMDatabaseQueueEx.h
@@ -0,0 +1,13 @@
+//
+//  FMDatabaseQueueEx.h
+//  OCEloquent
+//
+//  Created by lulinqing on 16/5/30.
+//  Copyright © 2016年 卢林清. All rights reserved.
+//
+
+#import <FMDB/FMDB.h>
+
+@interface FMDatabaseQueueEx : FMDatabase
+
+@end
---
+++ source/OCEloquent/OCEloquent/FMDatabaseQueueEx.m
@@ -0,0 +1,13 @@
+//
+//  FMDatabaseQueueEx.m
+//  OCEloquent
+//
+//  Created by lulinqing on 16/5/30.
+//  Copyright © 2016年 卢林清. All rights reserved.
+//
+
+#import "FMDatabaseQueueEx.h"
+
+@implementation FMDatabaseQueueEx
+
+@end
